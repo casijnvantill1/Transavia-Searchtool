5914ebf9dc9ab44879fc8f2731a8272ea61f7569
casijnvantill1 <casijn.van.till@hva.nl>
2024-12-12 18:24:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def search_anywhere_partial(df, search_term):
 st.title("Searchtool Transavia Aircraft Data")
 
 # Transavia-logo toevoegen
-st.image("data/hoi/transavia_logo.png", use_column_width=True)
+st.image("data/hoi/Transavia_logo.png", use_column_width=True)
 
 # Selecteer een bestand
 file_options = {
